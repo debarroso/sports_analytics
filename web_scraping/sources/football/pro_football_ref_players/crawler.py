a9e6c52a244d0cb5bcc55a42130f18bc20cbe1ac
debarroso <debarros.oliver@gmail.com>
2023-12-28 22:50:27 -0600

    remove unused imports

--- web_scraping/sources/football/pro_football_ref_players/crawler.py
+++ web_scraping/sources/football/pro_football_ref_players/crawler.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time, pathlib, platform, os
 import psycopg2
 
