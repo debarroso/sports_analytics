7c75986f39daa5edc86a8e8029c17222a19528d2
debarroso <debarros.oliver@gmail.com>
2024-01-20 16:16:31 -0600

    base db handler updates

--- library/classes/base_database_handler.py
+++ library/classes/base_database_handler.py
@@ -7,7 +7,23 @@ import csv
 class BaseDatabaseHandler:
     def __init__(
             self,
+            datalake_path = pathlib.Path(__file__).resolve().parents[2],
             db_config={},
-            schema=""
+            schema="",
+            table_name=""
         ):
-        pass
+        self.db_config = db_config
+        self.schema = schema
+        self.table_name = table_name
+
+        self.datalake_path = datalake_path
+
+    def get_postgres_connection(self):
+        return psycopg2.connect(**self.db_config)
+    
+    def copy_csv_to_postgres(self, csv_file, cursor):
+        with open(csv_file, "r") as f:
+            reader = csv.DictReader(f)
+            columns = [f"'{column}'" for column in reader.fieldnames]
+            f.seek(0)
+            cursor.copy_expert(f"COPY {self.schema}.{self.table_name} ({', '.join(columns)}) FROM STDIN WITH CSV HEADER", f)
